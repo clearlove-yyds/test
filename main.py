bf9efccf7dc7e5d3a698a6be726f38e976f35d86
clearlove-yyds <90022485+clearlove-yyds@users.noreply.github.com>
2021-11-26 20:40:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,7 +11,43 @@ def get_html(url):  # 获取url源码
     text = res.text
     return text
 
-def bupt_page(text, url):
+def hnu_page(text, url): #湖南大学
+    summer_info = {}
+    change_info = {}
+    soup = BeautifulSoup(text, 'lxml')
+
+    infos = soup.find_all('a')
+    #print(soup)
+    for info in infos:
+        title = info.text
+        url_href = info.get('href')
+        if "调剂" in title:
+            if "2021" in title:
+                change_info[title] = url + url_href
+        elif "夏令营" in title:
+            if "2021" in title:
+                summer_info[title] = url + url_href
+    return summer_info, change_info
+
+def uestc_page(text, url): #电子科技大学 ！！
+    summer_info = {}
+    change_info = {}
+    soup = BeautifulSoup(text, 'lxml')
+
+    infos = soup.find_all('a')
+    #print(soup)
+    for info in infos:
+        title = info.text
+        url_href = info.get('href')
+        if "调剂" in title:
+            if "2021" in title:
+                change_info[title] = url + url_href
+        elif "夏令营" in title:
+            if "2021" in title:
+                summer_info[title] = url + url_href
+    return summer_info, change_info
+
+def hust_page(text, url): #华中科技大学
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -27,7 +64,24 @@ def bupt_page(text, url):
                 summer_info[title] = url + url_href
     return summer_info, change_info
 
-def zju_page(text, url):
+def bupt_page(text, url): #北京邮电大学
+    summer_info = {}
+    change_info = {}
+    soup = BeautifulSoup(text, 'lxml')
+
+    infos = soup.find_all('a')
+    for info in infos:
+        title = info.text
+        url_href = info.get('href')
+        if "调剂" in title:
+            if "2021" in title:
+                change_info[title] = url + url_href
+        elif "夏令营" in title:
+            if "2021" in title:
+                summer_info[title] = url + url_href
+    return summer_info, change_info
+
+def zju_page(text, url): #浙江大学
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -46,7 +100,7 @@ def zju_page(text, url):
     return summer_info, change_info
 
 
-def ustc_page(text, url):  # 中科大咨询获取
+def ustc_page(text, url):  #中科大咨询获取
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -65,7 +119,7 @@ def ustc_page(text, url):  # 中科大咨询获取
     return summer_info, change_info
 
 
-def peking_page(text, url):
+def peking_page(text, url): #北京大学
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -86,7 +140,7 @@ def peking_page(text, url):
     return summer_info, change_info
 
 
-def tsinghua_page(text, url):  # 中科大咨询获取
+def tsinghua_page(text, url):  #清华大学
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -110,7 +164,7 @@ def tsinghua_page(text, url):  # 中科大咨询获取
     return summer_info, change_info
 
 
-def nudt_page(text, url):  # 中科大咨询获取
+def nudt_page(text, url):  #国防科技大学
     summer_info = {}
     change_info = {}
     soup = BeautifulSoup(text, 'lxml')
@@ -147,7 +201,6 @@ def package_make(school_, url, url_host, dict_summer_, dict_change_):
         tuple_temp = nudt_page(get_html(url), url_host)
         dict_summer_[school_] = tuple_temp[0]
         dict_change_[school_] = tuple_temp[1]
-
     elif school_ == "北邮":
         tuple_temp = bupt_page(get_html(url), url_host)
         dict_summer_[school_] = tuple_temp[0]
@@ -156,20 +209,34 @@ def package_make(school_, url, url_host, dict_summer_, dict_change_):
         tuple_temp = zju_page(get_html(url), url_host)
         dict_summer_[school_] = tuple_temp[0]
         dict_change_[school_] = tuple_temp[1]
+    elif school_ == "华科":
+        tuple_temp = hust_page(get_html(url), url_host)
+        dict_summer_[school_] = tuple_temp[0]
+        dict_change_[school_] = tuple_temp[1]
+    elif school_ == "成电":
+        tuple_temp = uestc_page(get_html(url), url_host)
+        dict_summer_[school_] = tuple_temp[0]
+        dict_change_[school_] = tuple_temp[1]
+    elif school_ == "湖大":
+        tuple_temp = hnu_page(get_html(url), url_host)
+        dict_summer_[school_] = tuple_temp[0]
+        dict_change_[school_] = tuple_temp[1]
 
 if __name__ == "__main__":
     dict_summer = {}
     dict_change = {}
-    url0 = "https://admission.pku.edu.cn/xly/index.htm?CSRFT=AQJ4-1F1G-N5T6-4SUM-YV8D-WCM1-LSWI-0Y2C"
-    url1 = "???"
+    url0 = "https://yz.uestc.edu.cn/zsxc1/xly.htm"
+    url1 = "https://yz.uestc.edu.cn"
 
-    school = "北大"
+    school = "成电"
     package_make(school, url0, url1, dict_summer, dict_change)
+    print(dict_summer)
+    print(dict_change)
 
     '''url0 = "https://admission.pku.edu.cn/xly/index.htm?CSRFT=AQJ4-1F1G-N5T6-4SUM-YV8D-WCM1-LSWI-0Y2C"  #
-    url1 = "https://yz.ustc.edu.cn"
-    school = "北大"
-    package_make(school, url0, url1, dict_summer, dict_change)'''
+        url1 = "https://yz.ustc.edu.cn"
+        school = "北大"
+        package_make(school, url0, url1, dict_summer, dict_change)'''
 
     '''url0 = "https://www.cs.tsinghua.edu.cn/zszp/zsxx.htm"  #
     url1 = "https://www.cs.tsinghua.edu.cn"
@@ -181,5 +248,23 @@ if __name__ == "__main__":
     url1 = "https://www.cs.tsinghua.edu.cn"
     school = "国科大"
     package_make(school, url0, url1, dict_summer, dict_change)'''
-    print(dict_summer)
-    print(dict_change)
+
+    '''url0 = "http://cs.hust.edu.cn/zxtz/24.htm"  #
+    url1 = "http://cs.hust.edu.cn"
+    school = "华科"
+    package_make(school, url0, url1, dict_summer, dict_change)'''
+
+    '''url0 = "http://www.cst.zju.edu.cn/32178/list.htm"  #
+    url1 = "http://www.cst.zju.edu.cn"
+    school = "浙大"
+    package_make(school, url0, url1, dict_summer, dict_change)'''
+
+    '''url0 = "https://scs.bupt.edu.cn/rcpy1/yjspy.htm"  #
+    url1 = "https://scs.bupt.edu.cn"
+    school = "北邮"
+    package_make(school, url0, url1, dict_summer, dict_change)'''
+
+    '''url0 = "http://csee.hnu.edu.cn/zsjy/sszs.htm"  #
+    url1 = "http://csee.hnu.edu.cn"
+    school = "湖大"
+    package_make(school, url0, url1, dict_summer, dict_change)'''
